e3a83d977ae64f8293a5d93d7e9a8a52d62be25b
Mootez <mootez@groopay.fr>
2023-10-29 04:32:39 +0100

    profiles

--- src/test/java/tn/esprit/kaddemproject/UniversityServiceImpTests.java
+++ src/test/java/tn/esprit/kaddemproject/UniversityServiceImpTests.java
@@ -34,6 +34,7 @@ public void testAddUniversite(){
     Universite savedUv= universiteService.addUniversite(uv);
     assertNotNull(savedUv.getNomUniv());
    assertEquals(ex+1,universiteService.retrieveAll().size());
+
     universiteService.delete(uv.getIdUniv());
    // assertNotNull(savedUv.getNomUniv());
 
