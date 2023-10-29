3335d8671f39d252e271fdf540f4c2eb48efae2e
Mootez <mootez@groopay.fr>
2023-10-29 03:41:30 +0100

    addxx

--- src/test/java/tn/esprit/kaddemproject/UniversityServiceImpTest.java
+++ src/test/java/tn/esprit/kaddemproject/UniversityServiceImpTest.java
@@ -7,9 +7,11 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import tn.esprit.kaddemproject.entities.Departement;
 import tn.esprit.kaddemproject.entities.Universite;
 import tn.esprit.kaddemproject.services.IUniversiteService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
@@ -23,10 +25,13 @@ IUniversiteService universiteService;
 
 @Test
 public void testAddUniversite(){
+    List<Departement> departementList=new ArrayList<>();
+    Departement departement=new Departement(1,"null",null);
+    departementList.add(departement);
 
     List<Universite> list=universiteService.retrieveAll();
     int ex=list.size();
-    Universite uv= new Universite(1,"university",null);
+    Universite uv= new Universite(1,"university",departementList);
 
 
     Universite savedUv= universiteService.addUniversite(uv);
