fd4c584295d650d287057db375277d106aa3094c
Mootez <mootez@groopay.fr>
2023-10-29 03:33:21 +0100

    add

--- src/test/java/tn/esprit/kaddemproject/UniversityServiceImpTest.java
+++ src/test/java/tn/esprit/kaddemproject/UniversityServiceImpTest.java
@@ -1,5 +1,6 @@
 
 package tn.esprit.kaddemproject;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -8,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import tn.esprit.kaddemproject.entities.Universite;
 import tn.esprit.kaddemproject.services.IUniversiteService;
+
+import java.util.List;
 
 @SpringBootTest
 public class UniversityServiceImpTest{
@@ -21,9 +24,15 @@ IUniversiteService universiteService;
 @Test
 public void testAddUniversite(){
 
+    List<Universite> list=universiteService.retrieveAll();
+    int ex=list.size();
     Universite uv= new Universite(1,"university",null);
+
+
     Universite savedUv= universiteService.addUniversite(uv);
-    assertNotNull(savedUv.getNomUniv());
+
+    assertEquals(ex+1,universiteService.retrieveAll().size());
+   // assertNotNull(savedUv.getNomUniv());
 
 }
 }
